1ed80ba3a6bd682e7a8abc19b6f2599f393bc0fe
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-08-08 23:11:41 +0300

    Create todo list using react classes and functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-//component file
-import TodoContainer from "./components/TodoContainer"
-ReactDOM.render(
-    <React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+// component file
+import TodoContainer from './functionBased/components/TodoContainer';
+
+// stylesheet
+import './functionBased/App.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
       <TodoContainer />
-    </React.StrictMode>,
-    document.getElementById("root")
-  )
+    </BrowserRouter>
+  </React.StrictMode>,
+);
