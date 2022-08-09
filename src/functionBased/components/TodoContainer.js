db1a1f55223255c9df876aa2ed934782b19a05c4
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-08-09 14:53:28 +0300

    Add routes and navbar

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import Navbar from "./Navbar"
 import { v4 as uuidv4 } from 'uuid';
 import Header from './Header';
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
+import { Route, Switch } from "react-router-dom";
+import About from "../pages/About";
+import NotMatch from "../pages/NotMatch";
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState(getInitialTodos());
@@ -59,6 +63,10 @@ const TodoContainer = () => {
   };
 
   return (
+    <>
+    <Navbar />
+    <Switch>
+    <Route exact path="/">
     <div className="container">
       <div className="inner">
         <Header />
@@ -71,6 +79,15 @@ const TodoContainer = () => {
         />
       </div>
     </div>
+  </Route>
+  <Route path="/about">
+      <About />
+    </Route>
+    <Route path="*">
+      <NotMatch />
+    </Route>
+  </Switch>
+  </>
   );
 };
 
